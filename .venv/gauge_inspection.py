d85be511d3c42ba2b4fe60c4e14e0a8b29faf078
Nikhil Sharma <98894357+nikhil-sharma-2610@users.noreply.github.com>
2024-07-04 19:55:21 +0530

    Update gauge_inspection.py

--- .venv/gauge_inspection.py
+++ .venv/gauge_inspection.py
@@ -6,7 +6,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='Meganikx@0',
+    password='',
     port='3306',
     database='gauge'
 )
